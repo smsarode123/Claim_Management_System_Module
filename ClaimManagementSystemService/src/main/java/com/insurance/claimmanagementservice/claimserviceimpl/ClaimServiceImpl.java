1c4d216c83a61fb386b1d3272a3ffce3702a2dcc
smssarode123 <sagarsarode1997@gmail.com>
2024-08-26 12:10:54 +0530

    save claim api added

--- ClaimManagementSystemService/src/main/java/com/insurance/claimmanagementservice/claimserviceimpl/ClaimServiceImpl.java
+++ ClaimManagementSystemService/src/main/java/com/insurance/claimmanagementservice/claimserviceimpl/ClaimServiceImpl.java
@@ -3,6 +3,7 @@ package com.insurance.claimmanagementservice.claimserviceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.insurance.claimmanagementservice.model.Claim;
 import com.insurance.claimmanagementservice.repository.ClaimRepository;
 import com.insurance.claimmanagementservice.service.ClaimService;
 
@@ -10,6 +11,12 @@ import com.insurance.claimmanagementservice.service.ClaimService;
 public class ClaimServiceImpl implements ClaimService {
 	
 	@Autowired private ClaimRepository repository;
+
+	@Override
+	public Claim saveClaimInformation(Claim claim) {
+		
+		return repository.save(claim);
+	}
 
 	
 }
--- ClaimManagementSystemService/src/main/java/com/insurance/claimmanagementservice/controller/ClaimController.java
+++ ClaimManagementSystemService/src/main/java/com/insurance/claimmanagementservice/controller/ClaimController.java
@@ -1,8 +1,13 @@
 package com.insurance.claimmanagementservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.insurance.claimmanagementservice.model.Claim;
 import com.insurance.claimmanagementservice.service.ClaimService;
 
 @RestController
@@ -11,7 +16,13 @@ public class ClaimController {
 	@Autowired
 	private ClaimService service;
 	
-	
+	@PostMapping("/saveClaimInformation")
+	public ResponseEntity<Claim> saveClaimInformation(@RequestBody Claim claim)
+	{
+		Claim claimRef = service.saveClaimInformation(claim);
+		
+		return new ResponseEntity<Claim>(claimRef, HttpStatus.CREATED);
+	}
 
 	
 
